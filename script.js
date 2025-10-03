04add4357599ad282a9bb21ed60b4655400c5b35
mani shankar <mmshankar0007@gmail.com>
2025-10-03 13:01:47 +0530

    added menu icon

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const navLinks=document.querySelectorAll('header nav a');
-const logoLink=document.querySelector('.logo');
-const sections = document.querySelectorAll('section')
+const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+});
 
 const activePage = () => {
     const header = document.querySelector('header');
@@ -23,7 +32,11 @@ const activePage = () => {
 
     sections.forEach(section => {
         section.classList.remove('active')
-    })
+    });
+
+    menuIcon.classList.add('fa-bars');
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
 }
 
 navLinks.forEach((link, idx) => {
