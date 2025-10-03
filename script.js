2bd580397f1c5cbdc29579c748abf536049b52c9
mani shankar <mmshankar0007@gmail.com>
2025-10-03 11:39:07 +0530

    added page transition effect

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const navLinks=document.querySelectorAll('header nav a');
+const logoLink=document.querySelector('.logo');
 const sections = document.querySelectorAll('section')
 
 const activePage = () => {
-    navLinks.forEach((link) => {
+    const header = document.querySelector('header');
+    const barsBox = document.querySelector('.bars-box');
+
+    header.classList.remove('active');
+    setTimeout(() => {
+        header.classList.add('active');
+    },1100);
+
+    navLinks.forEach(link => {
         link.classList.remove('active')
     })
-    sections.forEach((sect) => {
-        sect.classList.remove('active')
+
+    barsBox.classList.remove('active');
+    setTimeout(() => {
+        barsBox.classList.add('active');
+    },1100);
+
+
+    sections.forEach(section => {
+        section.classList.remove('active')
     })
 }
 
@@ -14,12 +30,31 @@ navLinks.forEach((link, idx) => {
     link.addEventListener('click',() => {
         if(!link.classList.contains('active')){
             activePage()
+
             link.classList.add('active')
-            sections[idx].classList.add('active')
+
+            setTimeout(()=>{
+                sections[idx].classList.add('active');
+            },1100);
+            // sections[idx].classList.add('active')
         }
         
     })
 })
+
+
+logoLink.addEventListener('click',() => {
+    if (!navLinks[0].classList.contains('active')){
+        activePage();
+
+        navLinks[0].classList.add('active');
+        
+        setTimeout(()=>{
+            sections[0].classList.add('active');
+        },1100);
+    }
+})
+
 
 
 const resumeBtns =document.querySelectorAll('.resume-btn');
